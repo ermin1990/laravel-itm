5d2d82e0015d13c15d64a2116f19daaa232f3d2e
erminselimovic <ermin1990@gmail.com>
2024-12-26 01:08:36 +0100

    Predavanje 22 -Vježba 2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get("/shop", [ShopController::class, "index"])->name("shop");
 //Contact routes
 Route::controller(ContactController::class)->group(function () {
     Route::get("/contact", "index")->name("contact");
-    Route::post("send-contact", "sendContact")->name("contact.send");
+    Route::post("/send-contact", "sendContact")->name("contact.send");
 });
 
 
@@ -23,21 +23,21 @@ Route::group(["prefix" => "admin"], function () {
 
     //Contact routes
     Route::controller(ContactController::class)->group(function () {
-        Route::get("contact", "allContacts")->name("allcontacts");
-        Route::get("delete-contact/{contact}", "delete")->name("deletecontact");
-        Route::get("edit-contact/{contact}", "edit")->name("editcontact");
-        Route::post("update-contact/{contact}", "update")->name("updatecontact");
+        Route::get("/contact/all", "allContacts")->name("allcontacts");
+        Route::get("/contact/delete/{contact}", "delete")->name("deletecontact");
+        Route::get("/contact/edit/{contact}", "edit")->name("editcontact");
+        Route::post("/contact/update/{contact}", "update")->name("updatecontact");
     });
 
 
     //Product routes
     Route::controller(ProductsController::class)->group(function () {
-        Route::get("products", "index")->name("admin.products");
-        Route::get("add-product", "create")->name("admin.addproduct");
-        Route::post("save-product", "store")->name("admin.saveproduct");
-        Route::get("delete-product/{product}", "deleteProduct")->name("admin.deleteproduct");
-        Route::get("edit-product/{product}", "edit")->name("admin.editproduct");
-        Route::post("update-product/{product}", "update")->name("admin.updateproduct");
+        Route::get("/products/all", "index")->name("admin.products");
+        Route::get("/products/add", "create")->name("admin.addproduct");
+        Route::post("/products/save", "store")->name("admin.saveproduct");
+        Route::get("/products/delete/{product}", "deleteProduct")->name("admin.deleteproduct");
+        Route::get("/products/edit/{product}", "edit")->name("admin.editproduct");
+        Route::post("/products/update/{product}", "update")->name("admin.updateproduct");
     });
 });
 
